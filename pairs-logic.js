9f5490e949871c8f54db7430e9fa37982cb68c3f
Lozing <lozingyt@gmail.com>
2023-03-10 11:55:26 +0000

    added flipping functionality

--- pairs-logic.js
+++ pairs-logic.js
@@ -1,5 +1,5 @@
 var cards = [];
-var shown = [];
+var selected = [];
 var numPairs = 3;
 var score = 0;
 var numToMatch = 2;
@@ -13,10 +13,17 @@ function changeSize(cards){
 function createCards(){
     // Find a random number generator and create pairs of cards with random features
     let currentCardNum = 0;
-    for(let i = 0; i < numPairs; i++){
-        let body = getRandomInt(3) + 1; //TODO Find if the combo of body eyes and mouth have been used?
-        let eyes = getRandomInt(3) + 1;
-        let mouth = getRandomInt(2) + 1;
+    let usedCombo = [];
+    let body;
+    let eyes;
+    let mouth;
+    for (let i = 0; i < numPairs; i++){
+        do {
+            body = getRandomInt(3) + 1; //TODO Find if the combo of body eyes and mouth have been used?
+            eyes = getRandomInt(3) + 1;
+            mouth = getRandomInt(2) + 1;
+        } while (usedCombo.includes([body, eyes, mouth]));
+        usedCombo.append([body, eyes, mouth]);
         let card1 = new Card(currentCardNum, body, eyes, mouth);
         let card2 = new Card(currentCardNum + 1, body, eyes, mouth);
 
@@ -32,20 +39,40 @@ function cardClick(num){
 }
 
 function render(){
-    // Design a way of rendering cards given the dimentions of the window
+    // Design a way of rendering cards given the dimentions of the window?
     //width = getWidth();
     for(let i = 0; i < numPairs * 2; i++){
         let newDiv = document.createElement('button');
         newDiv.className = "parent cardparent unstyled-button";
-        newDiv.innerHTML = cards[i].turnedString;
+        if (cards[i].flipped) {
+            newDiv.innerHTML = cards[i].turnedString;
+        }
+        else {
+            newDiv.innerHTML = cards[i].unTurnedString;
+        }
+        newDiv.onclick = cardClick(i);
         gameBoard.append(newDiv);
     }
 }
 
-function evalEqual(){
-    if(shown[0].isEqual(shown[1])){
+function evalEqual() {
+    matching = true;
+    let i = 1;
+    while (i < selected.length) {
+        if (!(selected[i].isEqual(selected[0].turnedString))) {
+            matching = false;
+            break;
+        }
+        else {
+            i++;
+        }
+    }
+
+    if(matching){
         // Keep shown and add score
+        score += 1;
     } else {
+        selected.forEach(function (c) { c.flipped = false; })
         // Turn over again
     }
 }
@@ -61,6 +88,7 @@ class Card {
                              <img src=\"../res/eyes" + this.eyes +".png\" class=\"playingcard image2\"/> \
                              <img src=\"../res/mouth" + this.mouth +".png\" class=\"playingcard image3\"/>";
         this.unTurnedString; // Create back of card icon
+        this.flipped = false;
     }
     
     isEqual(otherCard) {
@@ -71,12 +99,13 @@ class Card {
     }
 
     show() {
-        if(shown.length === 2){
+        selected.push(this);
+        this.flipped = true;
+        if (selected.length === numToMatch){
             evalEqual();
-            shown = [];
+            selected = [];
             return;
         }
-        shown.push(this);
         render();
     }
 }
